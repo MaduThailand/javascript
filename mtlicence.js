68f63e0b4d4c441ec2f4d7fff61860fb9fafcb24
MaduThailand <maduthailand.com@gmail.com>
2016-03-28 17:37:43 +0700

    Update mtlicence.js

--- mtlicence.js
+++ mtlicence.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
 	$(".deskripsi").html(deskripsi);
 	var harga = $(".harga").html();
 	$(".price").html(harga);
-	$(".hGfzU").html("Support By : <a href='http://www.maduthailand.com'>Madu Thailand</a>");
+	$(".hGfzU").html("Support By : <a href='http://www.maduthailand.com'>MaduThailand</a>");
 	$('a[href=#top]').click(function(){
         $('html, body').animate({scrollTop:0}, 'slow');
         return false;
